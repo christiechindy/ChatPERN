eb121f24f8000cecf641687c5b338b90e0da31db
christiechindy <chindychristie@gmail.com>
2024-06-25 16:00:31 +0800

    interface for socket

---
+++ client/src/socket-interface/interface.ts
@@ -0,0 +1,24 @@
+export interface ServerToClientEvents {
+    noArg: () => void;
+    basicEmit: (a: number, b: string, c: Buffer) => void;
+    withAck: (d: string, callback: (e: number) => void) => void;
+
+    receive_message: (data: IMessageData) => void;
+}
+
+export interface ClientToServerEvents {
+    // hello: () => void;
+    join_room: (relation_id: number) => void;
+    send_message: (messageData: IMessageData, relation_id: number) => void;
+}
+
+export interface InterServerEvents {
+    ping: () => void;
+}
+
+export interface IMessageData {
+    sent_time: string,
+    sender_id: number,
+    relation_id: number,
+    words: string,
+}
